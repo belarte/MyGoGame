de4774d0681f8383d1dbcd6d65b01a97914d8aa1
Bruno Belarte <bruno.belarte@gmail.com>
2015-07-10 20:55:28 +0200

    ADD: GetClosestEnemy

--- engine/ai/behaviour_tree.go
+++ engine/ai/behaviour_tree.go
@@ -5,15 +5,28 @@ import (
 	. "github.com/belarte/MyGoGame/engine/utils"
 )
 
+type charPosDist struct {
+	char *Character
+	pos  *Coord
+	dist float64
+}
+
 type Context struct {
-	level           *Level
-	agent           *Character
-	positionOfAgent Coord
-	visibleEnemies  []*Character
+	level                *Level
+	agent                *Character
+	positionOfAgent      Coord
+	visibleEnemies       []charPosDist
+	closestEnemy         *Character
+	closestEnemyPosition *Coord
 }
 
 func NewContext(level *Level, agent *Character) *Context {
-	return &Context{level, agent, level.PositionOf(agent), make([]*Character, 0, 4)}
+	return &Context{level, agent, level.PositionOf(agent), make([]charPosDist, 0, 4), nil, nil}
+}
+
+type Task interface {
+	CheckConditions()
+	Perform() bool
 }
 
 type GetVisibleEnemies struct {
@@ -30,7 +43,7 @@ func (self *GetVisibleEnemies) CheckConditions() bool {
 
 func (self *GetVisibleEnemies) Perform() bool {
 	opponents := self.context.level.GetOpponentsOf(self.context.agent)
-	self.context.visibleEnemies = make([]*Character, 0, 4)
+	self.context.visibleEnemies = make([]charPosDist, 0, 4)
 
 	if len(opponents) == 0 {
 		return false
@@ -40,9 +53,35 @@ func (self *GetVisibleEnemies) Perform() bool {
 		position := self.context.level.PositionOf(opponent)
 		distance := Distance(self.context.positionOfAgent, position)
 		if distance <= float64(self.context.agent.Visibility()) {
-			self.context.visibleEnemies = append(self.context.visibleEnemies, opponent)
+			char := charPosDist{opponent, &position, distance}
+			self.context.visibleEnemies = append(self.context.visibleEnemies, char)
 		}
 	}
 
 	return len(self.context.visibleEnemies) > 0
+}
+
+type GetClosestEnemy struct {
+	context *Context
+}
+
+func NewGetClosestEnemies(context *Context) *GetClosestEnemy {
+	return &GetClosestEnemy{context}
+}
+
+func (self *GetClosestEnemy) CheckConditions() bool {
+	return len(self.context.visibleEnemies) > 0
+}
+
+func (self *GetClosestEnemy) Perform() bool {
+	distance := 123456789.0
+	for _, opponent := range self.context.visibleEnemies {
+		if opponent.dist < distance {
+			self.context.closestEnemy = opponent.char
+			self.context.closestEnemyPosition = opponent.pos
+			distance = opponent.dist
+		}
+	}
+
+	return true
 }
--- engine/ai/behaviour_tree_test.go
+++ engine/ai/behaviour_tree_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestGetVisibleEnemiesCheckConditionsNilLevel(t *testing.T) {
 	char := NewCharacter("", 0, 0, 0, 0, 0)
-	context := &Context{nil, char, Coord{0, 0}, nil}
+	context := &Context{nil, char, Coord{0, 0}, nil, nil, nil}
 	task := NewGetVisibleEnemies(context)
 
 	if task.CheckConditions() {
@@ -18,7 +18,7 @@ func TestGetVisibleEnemiesCheckConditionsNilLevel(t *testing.T) {
 
 func TestGetVisibleEnemiesCheckConditionsNilCharacter(t *testing.T) {
 	level := NewLevel(Coord{0, 0}, 0)
-	context := &Context{level, nil, Coord{0, 0}, nil}
+	context := &Context{level, nil, Coord{0, 0}, nil, nil, nil}
 	task := NewGetVisibleEnemies(context)
 
 	if task.CheckConditions() {
@@ -74,5 +74,72 @@ func TestGetVisibleEnemiesPerformVisibleEnemies(t *testing.T) {
 
 	if !task.Perform() {
 		t.Errorf("Perform should be true, context=%+v", context)
+	}
+}
+
+func TestGetClosestEnemyCheckConditionsNoVisibleEnemies(t *testing.T) {
+	context := &Context{nil, nil, Coord{0, 0}, make([]charPosDist, 0, 0), nil, nil}
+	task := NewGetClosestEnemies(context)
+
+	if task.CheckConditions() {
+		t.Errorf("CheckConditions should return false, context=%+v", context)
+	}
+}
+
+func TestGetClosestEnemyCheckConditionsVisibleEnemies(t *testing.T) {
+	context := &Context{nil, nil, Coord{0, 0}, make([]charPosDist, 1, 1), nil, nil}
+	task := NewGetClosestEnemies(context)
+
+	if !task.CheckConditions() {
+		t.Errorf("CheckConditions should return true, context=%+v", context)
+	}
+}
+
+func TestGetClosestEnemyPerformOneEnemy(t *testing.T) {
+	char := NewCharacter("", 0, 0, 0, 0, 0)
+	position := Coord{0, 1}
+	opponents := []charPosDist{charPosDist{char, &position, 1}}
+	context := &Context{nil, nil, Coord{0, 0}, opponents, nil, nil}
+	task := NewGetClosestEnemies(context)
+
+	if !task.Perform() {
+		t.Errorf("Perform should be true, context=%+v", context)
+	}
+
+	if context.closestEnemy != char {
+		t.Errorf("Expected %+v, got %+v", char, context.closestEnemy)
+	}
+
+	if !EqualCoord(*(context.closestEnemyPosition), position) {
+		t.Errorf("Expected %+v, got %+v", position, context.closestEnemyPosition)
+	}
+}
+
+func TestGetClosestEnemyPerformThreeEnemies(t *testing.T) {
+	char1 := NewCharacter("char1", 0, 0, 0, 0, 0)
+	char2 := NewCharacter("char2", 0, 0, 0, 0, 0)
+	char3 := NewCharacter("char3", 0, 0, 0, 0, 0)
+	position1 := Coord{0, 1}
+	position2 := Coord{0, 2}
+	position3 := Coord{0, 3}
+
+	opponents := []charPosDist{
+		charPosDist{char3, &position3, 3},
+		charPosDist{char1, &position1, 1},
+		charPosDist{char2, &position2, 2},
+	}
+	context := &Context{nil, nil, Coord{0, 0}, opponents, nil, nil}
+	task := NewGetClosestEnemies(context)
+
+	if !task.Perform() {
+		t.Errorf("Perform should be true, context=%+v", context)
+	}
+
+	if context.closestEnemy != char1 {
+		t.Errorf("Expected %v, got %+v", char1, context.closestEnemy)
+	}
+
+	if !EqualCoord(*(context.closestEnemyPosition), position1) {
+		t.Errorf("Expected %+v, got %+v", position1, context.closestEnemyPosition)
 	}
 }
